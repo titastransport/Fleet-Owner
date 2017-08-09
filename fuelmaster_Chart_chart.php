bcab0e8d578c56f26a56f3de08d0869305f934f3
sharif khan <titastransport@gmail.com>
2017-08-09 05:58:19 -0400

    Add files via upload

---
+++ fuelmaster_Chart_chart.php
@@ -0,0 +1,206 @@
+<?php
+@ini_set("display_errors","1");
+@ini_set("display_startup_errors","1");
+
+require_once("include/dbcommon.php");
+require_once('include/xtempl.php');
+require_once('classes/chartpage.php');
+require_once('classes/searchclause.php');
+add_nocache_headers();
+
+require_once("include/fuelmaster_Chart_variables.php");
+
+if( !Security::processPageSecurity( $strtablename, 'S' ) )
+	return;
+
+
+
+
+
+$layout = new TLayout("chart_bootstrap3", "AvenueAvenue", "MobileAvenue");
+$layout->version = 3;
+	$layout->bootstrapTheme = "default";
+		$layout->customCssPageName = "fuelmaster_Chart_chart";
+$layout->blocks["center"] = array();
+$layout->containers["message"] = array();
+$layout->container_properties["message"] = array(  );
+$layout->containers["message"][] = array("name"=>"message",
+	"block"=>"message_block", "substyle"=>1  );
+
+$layout->skins["message"] = "";
+
+$layout->blocks["center"][] = "message";
+$layout->containers["form"] = array();
+$layout->container_properties["form"] = array(  );
+$layout->containers["form"][] = array("name"=>"chart",
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["form"] = "";
+
+$layout->blocks["center"][] = "form";
+$layout->blocks["left"] = array();
+$layout->containers["logo"] = array();
+$layout->container_properties["logo"] = array(  );
+$layout->containers["logo"][] = array("name"=>"logo",
+	"block"=>"logo_block", "substyle"=>1  );
+
+$layout->skins["logo"] = "";
+
+$layout->blocks["left"][] = "logo";
+$layout->containers["hdr"] = array();
+$layout->container_properties["hdr"] = array(  );
+$layout->containers["hdr"][] = array("name"=>"bsnavbarcollapse",
+	"block"=>"collapse_block", "substyle"=>1  );
+
+$layout->skins["hdr"] = "";
+
+$layout->blocks["left"][] = "hdr";
+$layout->containers["sidemenu"] = array();
+$layout->container_properties["sidemenu"] = array(  );
+$layout->containers["sidemenu"][] = array("name"=>"vmenu",
+	"block"=>"menu_block", "substyle"=>1  );
+
+$layout->skins["sidemenu"] = "";
+
+$layout->blocks["left"][] = "sidemenu";
+$layout->containers["left"] = array();
+$layout->container_properties["left"] = array(  );
+$layout->containers["left"][] = array("name"=>"searchpanel",
+	"block"=>"searchPanel", "substyle"=>1  );
+
+$layout->skins["left"] = "";
+
+$layout->blocks["left"][] = "left";
+$layout->blocks["top"] = array();
+$layout->containers["menu"] = array();
+$layout->container_properties["menu"] = array(  );
+$layout->containers["menu"][] = array("name"=>"wrapper",
+	"block"=>"", "substyle"=>1 , "container"=>"bc" );
+$layout->containers["bc"] = array();
+$layout->container_properties["bc"] = array(  );
+$layout->containers["bc"][] = array("name"=>"breadcrumbs",
+	"block"=>"breadcrumbs", "substyle"=>1  );
+
+$layout->skins["bc"] = "";
+
+
+$layout->containers["menu"][] = array("name"=>"wrapper",
+	"block"=>"", "substyle"=>1 , "container"=>"menu_1" );
+$layout->containers["menu_1"] = array();
+$layout->container_properties["menu_1"] = array(  );
+$layout->containers["menu_1"][] = array("name"=>"wrapper",
+	"block"=>"", "substyle"=>1 , "container"=>"more" );
+$layout->containers["more"] = array();
+$layout->container_properties["more"] = array(  );
+$layout->containers["more"][] = array("name"=>"morebutton",
+	"block"=>"more_list", "substyle"=>1  );
+
+$layout->containers["more"][] = array("name"=>"loggedas",
+	"block"=>"security_block", "substyle"=>1  );
+
+$layout->skins["more"] = "";
+
+
+$layout->containers["menu_1"][] = array("name"=>"wrapper",
+	"block"=>"", "substyle"=>1 , "container"=>"more_1" );
+$layout->containers["more_1"] = array();
+$layout->container_properties["more_1"] = array(  );
+$layout->containers["more_1"][] = array("name"=>"search",
+	"block"=>"searchform_block", "substyle"=>1  );
+
+$layout->skins["more_1"] = "";
+
+
+$layout->skins["menu_1"] = "";
+
+
+$layout->skins["menu"] = "";
+
+$layout->blocks["top"][] = "menu";
+$page_layouts["fuelmaster_Chart_chart"] = $layout;
+
+
+
+// add master layouts 
+
+
+$mode = postvalue("mode");
+if( $mode == "listdetails" )
+	$pageMode = CHART_DETAILS;
+elseif( $mode == "listdetailspopup" )
+	$pageMode = CHART_POPUPDETAILS;
+elseif( $mode == "dashchart" )
+	$pageMode = CHART_DASHBOARD;
+elseif( $mode == "dashdetails" )
+	$pageMode = CHART_DASHDETAILS;
+else
+	$pageMode = CHART_SIMPLE;
+
+$detailsMode = $pageMode != CHART_SIMPLE;
+
+$xt = new Xtempl( $detailsMode );
+
+$id = postvalue("id");
+$id = $id ? $id : 1;
+
+// set params for a RunnerPage constructor
+$params = array();
+$params["id"] = $id;
+$params["xt"] = &$xt;
+$params["mode"] = $pageMode;
+$params["tName"] = $strTableName;
+$params["pageType"] = PAGE_CHART;
+$params["masterTable"] = postvalue("mastertable");
+$params["isGroupSecurity"] = $isGroupSecurity; // a global variable
+$params["masterKeysReq"] = array();
+
+if( $pageMode = CHART_DASHBOARD )
+{
+	$params["dashElementName"] = postvalue("dashelement");
+	$params["dashTName"] = postvalue("table");
+}
+
+if( isset($_REQUEST["masterKeys"]) )
+	$masterKeys = my_json_decode( $_REQUEST["masterKeys"] );
+	
+$i = 0;
+while (true) 
+{
+	$i++;
+	if (isset($_REQUEST["masterkey".$i]))
+		$_masterKey = $_REQUEST["masterkey".$i];
+	elseif (isset($masterKeys["masterkey".$i]))
+		$_masterKey = $masterKeys["masterkey".$i];
+	else
+		break;
+	$params["masterKeysReq"][$i] = $_masterKey;
+}
+
+$pageObject = new ChartPage($params);
+$pageObject->init();
+
+// Save search processing
+if( postvalue("saveSearch") && postvalue("searchName") && !is_null($pageObject->searchLogger) ) 
+{
+	$searchName = postvalue("searchName");
+	$searchParams = $pageObject->getSearchParamsForSaving();
+	$pageObject->searchLogger->saveSearch( $searchName, $searchParams );
+	
+	$pageObject->searchClauseObj->savedSearchIsRun = true;
+	$_SESSION[$pageObject->sessionPrefix.'_advsearch'] = serialize( $pageObject->searchClauseObj );
+	
+	echo my_json_encode( $searchParams );
+	exit();
+}
+// Delete the saved search
+if( postvalue("deleteSearch") && postvalue("searchName") && !is_null($pageObject->searchLogger) ) 
+{
+	$searchName = postvalue("searchName");
+	$pageObject->searchLogger->deleteSearch( $searchName );
+	echo my_json_encode( array() );
+	exit();
+}
+
+$pageObject->process();
+
+?>
